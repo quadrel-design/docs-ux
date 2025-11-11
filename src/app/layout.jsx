6efcffeebaf2190d77a51335c1fdc9d43295bd72
grufio <info@grufio.com>
2025-11-11 11:16:23 +0100

    fix(build): import next/image in layout to prevent SSR 'Image is not defined'

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,6 +2,7 @@
 import { Footer, Layout, Navbar } from 'nextra-theme-docs'
 import { Head } from 'nextra/components'
 import { getPageMap } from 'nextra/page-map'
+import Image from 'next/image'
 import 'nextra-theme-docs/style.css'
 import 'yet-another-react-lightbox/styles.css'
 import 'yet-another-react-lightbox/plugins/captions.css'
