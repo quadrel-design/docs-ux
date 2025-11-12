e5c59bdd9218001911ee13efce2f36e7f0573712
grufio <info@grufio.com>
2025-11-12 22:28:05 +0100

    fix: hide theme switcher container area instead of just the button

---
+++ src/content/components/_meta.js
@@ -0,0 +1,4 @@
+export default {
+  index: 'Overview',
+  menus-actions: { title: 'Menu and Actions', display: 'children' }, // hide folder label; show its children only
+}
---
+++ src/content/components/menus-actions/_meta.js
@@ -0,0 +1,4 @@
+export default {
+  index: 'Create',            // the link you want
+  menu-actions: { display: 'hidden' } // hide all other children
+}
