6b7da3f9e765e736efd364ede258dd6e5d2b1c02
grufio <info@grufio.com>
2025-11-12 14:00:49 +0100

    content(floorplans): add section and meta

---
+++ src/content/floorplans/_meta.js
@@ -0,0 +1,13 @@
+export default {
+  index: { display: 'hidden' },
+  'get-started': '',
+  features: '',
+  themes: '',
+  pattern: 'Patterns',
+  test: '',
+  advanced: {
+    theme: {
+      copyPage: false
+    }
+  }
+}
