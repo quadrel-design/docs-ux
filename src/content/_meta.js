7f8b8f729a46c84882c731d575914b0986bc71e2
grufio <info@grufio.com>
2025-11-12 18:35:45 +0100

    chore: update content meta configuration

--- src/content/_meta.js
+++ src/content/_meta.js
@@ -3,4 +3,9 @@ export default {
   introduction: 'Introduction',
   floorplans: 'Floorplans',
   components: 'Components',
+  patterns: 'Patterns',
+  shell: 'Shell',
+  compositions: 'Compositions',
+  dos-and-donts: 'Dos and Donts',
+  tools: 'Tools',
 }
