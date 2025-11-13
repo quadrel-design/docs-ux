8d1a6f8cd60e420d397ee2bf49cfa6329dde5369
grufio <info@grufio.com>
2025-11-13 13:44:46 +0100

    fix: Remove QuadrelHeaderConfig from mdx-components

--- mdx-components.js
+++ mdx-components.js
@@ -2,7 +2,6 @@ import { useMDXComponents as getDocsMDXComponents } from 'nextra-theme-docs'
 import QuadrelLightbox from './src/components/QuadrelLightbox'
 import QuadrelMasonryGallery from './src/components/QuadrelMasonryGallery'
 import QuadrelCard, { QuadrelCardGrid } from './src/components/QuadrelCard'
-import QuadrelHeaderConfig from './src/components/QuadrelHeaderConfig'
 import QuadrelMermaid from './src/components/QuadrelMermaid'
 
 const docsComponents = getDocsMDXComponents()
@@ -13,7 +12,6 @@ export const useMDXComponents = components => ({
   QuadrelMasonryGallery,
   QuadrelCard,
   QuadrelCardGrid,
-  QuadrelHeaderConfig,
   Mermaid: QuadrelMermaid, // Override default Mermaid component with custom styling
   ...components
 })
