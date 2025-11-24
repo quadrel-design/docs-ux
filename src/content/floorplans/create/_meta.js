317540daabc49213e4e34a76802a8ffe343bd041
grufio <info@grufio.com>
2025-11-24 06:00:38 +0100

    Refine floorplans create multi-stepper/single-stepper and clean default assets

--- src/content/floorplans/create/_meta.js
+++ src/content/floorplans/create/_meta.js
@@ -1,4 +1,5 @@
 export default {
   index: 'Create',            // the link you want
   'multi-stepper': { display: 'hidden' } // hide all other children
+  'single-stepper': { display: 'hidden' } // hide all other children
 }
