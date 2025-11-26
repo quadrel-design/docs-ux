c914b49860facdaffeff508e0ee0cd771409a830
grufio <info@grufio.com>
2025-11-26 15:39:44 +0100

    docs: update floorplans content/meta

--- src/content/floorplans/_meta.js
+++ src/content/floorplans/_meta.js
@@ -1,5 +1,3 @@
 export default {
   index: 'Overview',
-  save: { title: 'Save', display: 'children' }, // hide folder label; show its children only
-  inspect: { title: 'Inspect', display: 'children' }, // hide folder label; show its children only
 }
