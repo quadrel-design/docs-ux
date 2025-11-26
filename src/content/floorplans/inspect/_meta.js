fb92bb36d549b472acce0fb301c4355665a1ef7e
grufio <info@grufio.com>
2025-11-26 15:18:43 +0100

    docs: add floorplans inspect save/submit pages and assets; update index

---
+++ src/content/floorplans/inspect/_meta.js
@@ -0,0 +1,5 @@
+export default {
+  index: 'Inspect',            // the link you want
+  'save': { display: 'hidden' }, // hide all other children
+  'submit': { display: 'hidden' } // hide all other children
+}
